5e331279d8f93e84c71d4b288e9a3f085524dcf3
Alex Smile <alexsmile39@gmail.com>
2017-02-16 23:09:07 +0300

    added protetction height

--- mario_pyramid.c
+++ mario_pyramid.c
@@ -6,12 +6,20 @@
 int main(void)
 {
     int h, tmp;//h - column high, tmp-temporary
-    int i, j;// i - counter, j - key to enter the cicle
+    int i, j = false;// i - counter, j - key to enter the cicle
     
 	printf("please enter the height of the columne\nheight of the columne = ");
-	scanf("%d",&h);
-	//h = GetInt();
-    tmp = h;
+	
+	while(j == false)
+	{
+		scanf("%d",&h);
+		//h = GetInt();
+			if(h < 0 || h > 23)
+				printf("wrong, height can not be < 0 or > 23, try again\n");
+			else
+				j = true;
+	}
+	tmp = h;
     for(i = 1; j == true; i++)
     {
         if(i < h)
